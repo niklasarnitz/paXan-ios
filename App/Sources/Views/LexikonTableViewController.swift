e29cb269cbea31929570226167b06bf7cf8f2c68
Niklas Arnitz <niklas.arnitz@jamitlabs.com>
2019-08-31 18:14:19 +0200

    Fixed a few Names

--- App/Sources/Views/LexikonTableViewController.swift
+++ App/Sources/Views/LexikonTableViewController.swift
@@ -21,6 +21,10 @@ class LexikonTableViewController: UITableViewController {
         LexikonEntry(
             title: "Jesus",
             description: "Gottes Sohn, Retter aller, welche an ihn glauben"
+        ),
+        LexikonEntry(
+            title: "SWDEC",
+            description: "Der SWDEC ist ein Unterverband der Weltweiten EC-Bewegung.\nEs is ein Verband christlicher Jugendarbeiten, bei denen Jesus Christus im Mittelpunkt des Lebens der Mitglieder und der Gemeinden steht.\nDer SWDEC arbeitet ebenfalls eng mit den Liebenzeller Gemeinschaften und der Evangelischen Landeskirche zusammen."
         )
     ]
 
