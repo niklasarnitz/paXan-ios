44da88abe9b3000f0030e4ab1621c6c3bee9a876
Niklas Arnitz <niklas.arnitz@jamitlabs.com>
2019-10-23 18:58:44 +0200

    Update Dependencies
    Add SwiftyUserDefaults for easier use of UserDefaults

--- Package.swift
+++ Package.swift
@@ -7,12 +7,14 @@ let package = Package(
     dependencies: [
         // add your dependencies here, for example:
 	.package(url: "git@github.com:SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.0")),
+	.package(url: "git@github.com:sunshinejr/SwiftyUserDefaults.git", .upToNextMajor(from: "5.0.0-beta.5")),
     ],
     targets: [
         .target(
             name: "paXan",
             dependencies: [
 		"SnapKit",
+		"SwiftyUserDefaults"
             ],
             path: "App"
         ),
