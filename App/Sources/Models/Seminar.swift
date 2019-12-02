618b66b8aaaafbd0be97945132ff72473e71cef1
Niklas Arnitz <niklas.arnitz@jamitlabs.com>
2019-12-03 00:23:53 +0100

    Add explanatory Content

--- App/Sources/Models/Seminar.swift
+++ App/Sources/Models/Seminar.swift
@@ -3,10 +3,15 @@
 import Foundation
 import SwiftyUserDefaults
 
+/// The struct depicting a Seminar
 struct Seminar {
+    /// The Seminar's title
     var title: String?
+    /// The organizer of the Seminar
     var referent: String?
+    /// A short (or long) description of the Seminar
     var description: String?
+    /// The Location Data for the Seminar
     var place: Place?
 
     init?(title: String?, referent: String?, description: String?, setPlace: Place?) {
