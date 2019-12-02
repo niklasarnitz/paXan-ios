b6347290f84b3f4945576bcb4fb1978ad0474ab4
Niklas Arnitz <niklas.arnitz@jamitlabs.com>
2019-12-03 00:23:21 +0100

    Adjust animation of button.pulsate()

--- App/Sources/Extensions/UIButtonExtension.swift
+++ App/Sources/Extensions/UIButtonExtension.swift
@@ -9,7 +9,7 @@ extension UIButton {
         pulse.duration = 0.4
         pulse.fromValue = 0.98
         pulse.toValue = 1.0
-        pulse.autoreverses = true
+        pulse.autoreverses = false
         pulse.repeatCount = .zero
         pulse.initialVelocity = 0.5
         pulse.damping = 1.0
